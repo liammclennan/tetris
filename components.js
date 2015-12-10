a2a54dde97b76aa613a9db4359f34f3a46b53b75
Liam McLennan <liam.mclennan@gmail.com>
2015-12-10 15:44:19 +1000

    went all es2015

---
+++ components.js
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+export var Square = React.createClass({
+    render: function() {
+    		var s = {
+        	left: (this.props.col-1) * 25 + 'px',
+          top: (this.props.row-1) * 25 + 'px'
+        };
+        return <div className="square" style={s}></div>;
+    }
+});
+
+export var OShape = React.createClass({
+	render: function () {
+  	return <div>
+    	<Square row={this.props.row} col={this.props.col} />
+  bb		<Square row={this.props.row} col={this.props.col+1} />
+      <Square row={this.props.row+1} col={this.props.col} />
+  		<Square row={this.props.row+1} col={this.props.col+1} />
+    </div>;
+  }
+});
+
+export var LShape = React.createClass({
+	render: function () {
+  	return <div>
+    	<Square row={this.props.row} col={this.props.col} />
+  		<Square row={this.props.row+1} col={this.props.col} />
+      <Square row={this.props.row+2} col={this.props.col} />
+  		<Square row={this.props.row+3} col={this.props.col} />
+    </div>;
+  }
+});
+
+export var SShape = React.createClass({
+	render: function () {
+  	return <div>
+    	<Square row={this.props.row} col={this.props.col+1} />
+  		<Square row={this.props.row} col={this.props.col+2} />
+      <Square row={this.props.row+1} col={this.props.col} />
+  		<Square row={this.props.row+1} col={this.props.col+1} />
+    </div>;
+  }
+});
