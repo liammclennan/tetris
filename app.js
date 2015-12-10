9f2c695d4e766d03d9280f086dd5429520cd03e3
Liam McLennan <liam.mclennan@gmail.com>
2015-12-11 08:59:25 +1000

    setInterval

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Components = require('./components');
-var Model = require('./model');
+import * as React from 'react';
+import * as ReactDOM  from 'react-dom';
+import * as Components from './components';
+import * as Model from './model';
+import { createStore } from 'redux'
 
 var data = [new Model.O(1,1), new Model.L(1,4)];
 
+function reducer(state = [], action) {
+  switch (action.type) {
+    case 'TICK':
+      state.push(new Model.O(action.data*2,action.data*2));
+      return state;
+    default: return state;
+  }
+  return [new Model.O(1,1), new Model.L(1,4)];
+}
 
-ReactDOM.render(
-<div>
-  {data.map(c => <Components.ShapeView shape={c} />)}
-</div>,
-    document.getElementById('container')
-);
+let store = createStore(reducer);
+store.subscribe(() => {
+  console.dir(store.getState());
+  ReactDOM.render(
+  <div>
+    {store.getState().map(c => <Components.ShapeView shape={c} />)}
+  </div>,
+      document.getElementById('container')
+  );
+});
+var counter = 1;
+setInterval(() => store.dispatch({ type: 'TICK', data: counter++ }),1000);
