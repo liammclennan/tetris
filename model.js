b01c12dd887c3f86e21a3a66a24f727627b1d171
Liam McLennan <liam.mclennan@gmail.com>
2015-12-21 15:24:47 +1000

    all pieces rotating

--- model.js
+++ model.js
@@ -17,16 +17,14 @@ export class Point {
 
 // a tetromino
 export class Shape {
-  constructor(name, points, rotator) {
+  constructor(name, rotator) {
     this.name = name;
-    this.points = points;
     this.rotator = rotator;
   }
   at(point) {
     return new Piece(this, point);
   }
   pointsRotated(rotation) {
-    if (!this.rotator) return this.points;
     return this.rotator(rotation);
   }
 }
@@ -66,11 +64,10 @@ export class Game {
     this.rubble = [];
   }
   tick() {
-    if (this.fallingPiece.maxRow() == this.rows) {
-      this.convertToRubble();
-      return this;
-    }
     this.fallingPiece.offset = this.fallingPiece.offset.fallOne();
+    if (this.fallingPiece.maxRow() >= this.rows) {
+      this.convertToRubble();
+    }
     return this;
   }
   convertToRubble() {
@@ -88,16 +85,40 @@ export class Game {
 
 // dictionary of shape type to square offsets
 export var shapes = {
-  'O': new Shape('O', [new Point(1,1),new Point(1,2), new Point(2,1),new Point(2,2)], rotation => [new Point(1,1),new Point(1,2), new Point(2,1),new Point(2,2)]),
-  'I': new Shape('I', [new Point(1,1), new Point(2,1),new Point(3,1), new Point(4,1)], rotation => {
+  'O': new Shape('O', rotation => [new Point(1,1),new Point(1,2), new Point(2,1),new Point(2,2)]),
+  'I': new Shape('I', rotation => {
     switch (rotation) {
       case 'N': return [new Point(1,1), new Point(2,1),new Point(3,1), new Point(4,1)];
       case 'E': return [new Point(2,1), new Point(2,2),new Point(2,3), new Point(2,4)];
+      case 'S': return [new Point(1,1), new Point(2,1),new Point(3,1), new Point(4,1)];
+      case 'W': return [new Point(2,1), new Point(2,2),new Point(2,3), new Point(2,4)];
     }
   }),
-  'T': new Shape('T', [new Point(1,1), new Point(1,2), new Point(2,2), new Point(1,3)]),
-  'L': new Shape('L', [new Point(1,1), new Point(2,1), new Point(1,2), new Point(1,3)]),
-  'Z': new Shape('Z', [new Point(1,1), new Point(1,2), new Point(2,2), new Point(2,3)])
+  'T': new Shape('T', rotation => {
+    switch (rotation) {
+      case 'N': return [new Point(1,1), new Point(1,2), new Point(2,2), new Point(1,3)];
+      case 'E': return [new Point(1,2), new Point(2,2),new Point(3,2), new Point(2,1)];
+      case 'S': return [new Point(1,2), new Point(2,1),new Point(2,2), new Point(2,3)];
+      case 'W': return [new Point(1,1), new Point(2,1),new Point(3,1), new Point(2,2)];
+    }
+  }),
+  'L': new Shape('L', rotation => {
+    switch (rotation) {
+      case 'N': return [new Point(1,1), new Point(2,1), new Point(1,2), new Point(1,3)];
+      case 'E': return [new Point(1,1), new Point(1,2), new Point(2,2), new Point(3,2)];
+      case 'S': return [new Point(1,3), new Point(2,1), new Point(2,2), new Point(2,3)];
+      case 'W': return [new Point(1,1), new Point(2,1), new Point(3,1), new Point(3,2)];
+
+    }
+  }),
+  'Z': new Shape('Z', rotation => {
+    switch (rotation) {
+      case 'N': return [new Point(1,1), new Point(1,2), new Point(2,2), new Point(2,3)];
+      case 'E': return [new Point(1,2), new Point(2,2),new Point(2,1), new Point(3,1)];
+      case 'S': return [new Point(1,1), new Point(1,2), new Point(2,2), new Point(2,3)];
+      case 'W': return [new Point(1,2), new Point(2,2),new Point(2,1), new Point(3,1)];
+    }
+  })
 };
 shapes.selectRandom = function() {
   var index = Math.floor(Math.random()*1000000%5);
--- test/modelsTests.js
+++ test/modelsTests.js
@@ -53,17 +53,18 @@ describe('models', function () {
         piece.rotate();
         assert(pieceHasPoints(piece, [new Models.Point(2,1),new Models.Point(2,2),new Models.Point(2,3),new Models.Point(2,4)]));
       });
-    });
-  });
-});
-
-
-
-describe('Array', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
+      it('should rotate to the correct S position', ()=> {
+        piece.rotate();
+        assert(pieceHasPoints(piece, [new Models.Point(1,1),new Models.Point(2,1),new Models.Point(3,1),new Models.Point(4,1)]));
+      });
+      it('should rotate to the correct W position', ()=> {
+        piece.rotate();
+        assert(pieceHasPoints(piece, [new Models.Point(2,1),new Models.Point(2,2),new Models.Point(2,3),new Models.Point(2,4)]));
+      });
+      it('should rotate back to N', ()=> {
+        piece.rotate();
+        assert(pieceHasPoints(piece, [new Models.Point(1,1),new Models.Point(2,1),new Models.Point(3,1),new Models.Point(4,1)]));
+      });
     });
   });
 });
