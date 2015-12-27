ce0c318f9d85ff7cf4edd05cc255395da0bd7294
Liam McLennan <liam.mclennan@gmail.com>
2015-12-27 19:20:48 +1000

    Renamed shape

--- model.js
+++ model.js
@@ -12,8 +12,7 @@ export class Point {
   }
 }
 
-// a tetromino
-export class Shape {
+export class Tetromino {
   constructor(name, rotator) {
     this.name = name;
     this.rotator = rotator;
@@ -126,8 +125,8 @@ export class Game {
 
 // dictionary of shape type to square offsets
 export var shapes = {
-  'O': new Shape('O', rotation => [new Point(1,1),new Point(1,2), new Point(2,1),new Point(2,2)]),
-  'I': new Shape('I', rotation => {
+  'O': new Tetromino('O', rotation => [new Point(1,1),new Point(1,2), new Point(2,1),new Point(2,2)]),
+  'I': new Tetromino('I', rotation => {
     switch (rotation) {
       case 'N': return [new Point(1,1), new Point(2,1),new Point(3,1), new Point(4,1)];
       case 'E': return [new Point(2,1), new Point(2,2),new Point(2,3), new Point(2,4)];
@@ -135,7 +134,7 @@ export var shapes = {
       case 'W': return [new Point(2,1), new Point(2,2),new Point(2,3), new Point(2,4)];
     }
   }),
-  'T': new Shape('T', rotation => {
+  'T': new Tetromino('T', rotation => {
     switch (rotation) {
       case 'N': return [new Point(1,1), new Point(1,2), new Point(2,2), new Point(1,3)];
       case 'E': return [new Point(1,2), new Point(2,2),new Point(3,2), new Point(2,1)];
@@ -143,7 +142,7 @@ export var shapes = {
       case 'W': return [new Point(1,1), new Point(2,1),new Point(3,1), new Point(2,2)];
     }
   }),
-  'L': new Shape('L', rotation => {
+  'L': new Tetromino('L', rotation => {
     switch (rotation) {
       case 'N': return [new Point(1,1), new Point(2,1), new Point(1,2), new Point(1,3)];
       case 'E': return [new Point(1,1), new Point(1,2), new Point(2,2), new Point(3,2)];
@@ -152,7 +151,7 @@ export var shapes = {
 
     }
   }),
-  'Z': new Shape('Z', rotation => {
+  'Z': new Tetromino('Z', rotation => {
     switch (rotation) {
       case 'N': return [new Point(1,1), new Point(1,2), new Point(2,2), new Point(2,3)];
       case 'E': return [new Point(1,2), new Point(2,2),new Point(2,1), new Point(3,1)];
